bf67aa0bd0b489cef3bdbcf0a686030921661fa8
Carson Schaefer <schaefca@mail.gvsu.edu>
2016-04-03 15:34:16 -0400

    GETTING FOREGROUND ACTIVITY AND STARTING TEST 4 TIME APP

--- app/src/main/java/com/test4time/test4time/DeviceIntentService.java
+++ app/src/main/java/com/test4time/test4time/DeviceIntentService.java
@@ -3,7 +3,10 @@ package com.test4time.test4time;
 import android.app.ActivityManager;
 import android.app.IntentService;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.os.*;
+import android.os.Process;
 import android.util.Log;
 
 import java.util.List;
@@ -21,18 +24,32 @@ public class DeviceIntentService extends IntentService {
         super("DeviceIntentService");
     }
 
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
+    }
+
     /*
-     *
-     */
+         *
+         */
     @Override
     protected void onHandleIntent(Intent intent) {
        // what the service does
         ActivityManager manager = (ActivityManager) getApplicationContext().getSystemService(ACTIVITY_SERVICE);
-        List< ActivityManager.RunningTaskInfo > runningTaskInfo = manager.getRunningTasks(1);
-        while(true) {
-            ComponentName componentInfo = runningTaskInfo.get(0).topActivity;
-            if(componentInfo.getPackageName().equals("")) {
 
+        while(true) {
+            List<ActivityManager.RunningAppProcessInfo> tasks = manager.getRunningAppProcesses();
+            Log.d("FOREGROUND",tasks.get(0).processName); //
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if(tasks.get(0).processName.equals("com.pandora.android")) { // get foreground activity
+                Intent test4Time = new Intent(getApplicationContext(), BlockedApps.class);
+                test4Time.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(test4Time);
             }
         }
     }
