0d2ad3d5d62adc74c371eb69eb7435c0053677bd
ZachCornell <ibrethhocke91@gmail.com>
2016-04-07 20:35:53 -0400

    Users intent working

---
+++ app/src/main/java/com/test4time/test4time/User.java
@@ -0,0 +1,34 @@
+package com.test4time.test4time;
+
+/**
+ * Created by Zach on 4/6/16.
+ */
+public class User {
+    private String name, grade, time;
+
+
+    public User(){
+
+    }
+    public User(String name, String grade, String time) {
+        this.name = name;
+        this.grade = grade;
+        this.time = time;
+    }
+
+    public void setName(String name) {
+        this.name = db.getString(1);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPackageName() {
+        return grade;
+    }
+
+    public String getProcessName() {
+        return time;
+    }
+}
---
+++ app/src/main/java/com/test4time/test4time/UsersViewHolder.java
@@ -0,0 +1,26 @@
+/*package com.test4time.test4time;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+*//**
+ * Created by Zach on 4/6/16.
+ *//*
+public class UsersViewHolder extends RecyclerView.ViewHolder{
+    protected ImageButton settingsBtn;
+    protected ImageView pencila;
+    protected ImageView pencilb;
+    protected TextView usersText;
+
+    public UsersViewHolder(View view) {
+        super(view);
+        this.pencila = (ImageView) view.findViewById(R.id.pencil2);
+        this.pencilb = (ImageView) view.findViewById(R.id.pencil);
+        this.usersText = (TextView) view.findViewById(R.id.UsersText);
+        this.settingsBtn = (ImageButton) view.findViewById(R.id.settingsBtn);
+        view.setClickable(true);
+    }
+}*/
